ab2508f5735c01f520b7b93084c8b0d402552293
Diego Matias <dg.matias021@gmail.com>
2022-11-13 15:35:17 -0300

    Add files via upload

---
+++ main/home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> Home </title>
+</head>
+<body>
+    
+
+    <div> <a href=""> Sistema de estacionamento </a> </div>
+        
+    <div> <img src="" alt="foto de perfil"></div>
+    <div> <a href="logout.php"></a> </div>
+
+    <div> <a href=""> Perfil do operador </a> </div>
+    <div> <a href=""> Cadastro de veículo </a> </div>
+
+    <form action="" method="get">
+
+        <label for="">
+
+            Pesquisar:
+            <input type="text">
+            <input type="text">
+            <input type="submit" value="Buscar">
+        </label>
+
+    </form>
+
+</body>
+</html>
