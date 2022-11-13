a428cd65d4188c4784d79448b65d96a0f17ff24e
quebec <85215097+jcvieiraa@users.noreply.github.com>
2022-11-13 18:09:06 -0300

    Add files via upload

--- main/vehicleregistration.php
+++ main/vehicleregistration.php
@@ -29,7 +29,9 @@
     <h1>Cadastro de Veículo</h1>
 
     <form action="" method="post" class="form-edit">
+
         <input type="hidden" name="operador">
+
         <div class="edit">
             <label for="" class="form-label">
                 Cliente:
@@ -42,6 +44,7 @@
                 <input type="text" name="contato" class="form-control">
             </label>
         </div>
+
         <div class="edit">
             <label for="" class="form-label">
                 Veículo:
@@ -52,36 +55,42 @@
                     <option value="3">Pobre</option>
             </label>
         </div>
+
         <div class="edit">
             <label for="" class="form-label">
                 Placa:
                 <input type="text" name="placa" class="form-control">
             </label>
         </div>
+
         <div class="edit">
             <label for="" class="form-label">
                 Marca:
                 <input type="text" name="marca" class="form-control">
             </label>
         </div>
+
         <div class="edit">
             <label for="" class="form-label">
                 Modelo:
                 <input type="text" name="modelo" class="form-control">
             </label>
         </div>
+
         <div class="edit">
             <label for="" class="form-label">
                 Data:
                 <input type="date" name="data" class="form-control">
             </label>
         </div>
+
         <div class="edit">
             <label for="" class="form-label">
                 Hora:
                 <input type="time" name="hora" class="form-control">
             </label>
         </div>
+
         <input type="submit" class="btn btn-primary" name="Cadastrar" value="Cadastrar">
         <a href="index.php" class="btn btn-danger">Cancelar e voltar</a>
     </form>
