91ba59dd2f7cab43d06e5b8c2e955237a40dec4b
Diego Matias <dg.matias021@gmail.com>
2022-11-14 13:24:20 -0300

    Add files via upload

---
+++ main/config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+//arquivo de configuração para conectar o banco de dados aos arquivos
+
+$db_name = "estacionamento";
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+
+$pdo = new PDO("mysql:dbname=".$db_name.";host=".$db_host, $db_user, $db_pass);
