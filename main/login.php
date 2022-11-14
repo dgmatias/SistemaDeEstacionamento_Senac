811b20c79c532b4f7d37ceba048c2f54aa5dc993
Frederico Gondin <108304623+fgondin@users.noreply.github.com>
2022-11-14 19:25:41 -0300

    Update login.php

--- main/login.php
+++ main/login.php
@@ -1,14 +1,27 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
+<div class="container">
 
-    <h1>Parabéns!</h1>
-    
-</body>
-</html>
+    <h1>login<h1>
+
+    <form action="login_action.php" method="post">
+
+        <div class="mb-3">
+            <label for="" class="form-label">
+                Email: <br>
+                <input type="email"
+                name="email"
+                class="form-control"
+                value="<?php if(isset($dados['email'])){echo $dados['email'];}?>"
+                >
+            </label>
+        </div>
+        <div class="mb-3">
+            <label for="" class="form-label">
+                Senha: <br>
+                <input type="password" name="senha" class="form-control">
+            </label>
+        </div>
+
+        <input type="submit" value="Salvar" name="SendLogin" class="btn btn-primary"/>
+    </form>
+    <h5><a type="button" class="btn btn-link" href="cadastro.php">Cadastrar</a></h5>
+</div>
