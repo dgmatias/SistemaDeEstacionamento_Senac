01a98cc798eabd9147aec8703a21c277926457f3
Thiago <47253697+ThiagoP08@users.noreply.github.com>
2022-11-14 22:05:28 -0300

    Update logout.php

--- main/logout.php
+++ main/logout.php
@@ -5,5 +5,5 @@ unset($_SESSION['id'], $_SESSION['nome']);  //apaga a sessão
 
 $_SESSION['msg'] = "<p style='color: green'>Deslogado com sucesso!</p>";    //mensagem de confirmação
 
-header("Location: login.php");  //Redireciona para a página "index.php"
+header("Location: login.php");  //Redireciona para a página "login.php"
 exit;
