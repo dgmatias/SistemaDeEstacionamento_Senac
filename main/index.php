58a7d01a27b65f23d0db12e7e40f9ba0c7ad3f63
quebec <85215097+jcvieiraa@users.noreply.github.com>
2022-11-16 18:41:45 -0300

    Add files via upload

--- main/index.php
+++ main/index.php
@@ -1,23 +1,7 @@
 <?php
-    session_start();
-    ob_start();
-    require 'config.php';
+session_start();
+ob_start();
+unset($_SESSION['id'], $_SESSION['nome']);  //apaga a sessão
 
-    if(isset($_SESSION['id'], $_SESSION['nome'])) {
-        header("Location: home.php");
-        exit;
-    }
-?>
-
-<body style="padding: 4rem">
-
-    <div>
-        <h1> Seja bem-vindo! </h1><br/>
-    </div>
-
-    <div>
-        <a href="login.php"> Login </a><br/><br/>
-        <a href="cadastrar.php"> Criar conta </a>
-    </div>
-
-</div>
+header("Location: login.php");  //Redireciona para a página "login.php"
+exit;
