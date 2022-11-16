db79703c2aeeabb87876b10a8fe2f5e20df384cf
Diego Matias <dg.matias021@gmail.com>
2022-11-15 21:55:15 -0300

    Delete logout.php

--- main/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-ob_start();
-unset($_SESSION['id'], $_SESSION['nome']);  //apaga a sessão
-
-$_SESSION['msg'] = "<p style='color: green'>Deslogado com sucesso!</p>";    //mensagem de confirmação
-
-header("Location: login.php");  //Redireciona para a página "login.php"
-exit;
