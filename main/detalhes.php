bf4edb31a58cfc711b7a3697919b278b2f8c02a9
Thiago <47253697+ThiagoP08@users.noreply.github.com>
2022-11-16 15:48:10 -0300

    Add files via upload

--- main/detalhes.php
+++ main/detalhes.php
@@ -8,21 +8,23 @@ require 'config.php';
 
 $lista = [];
 
-$sql = $pdo->query("SELECT * FROM tbl_veiculo");
-$lista = $sql->fetchall(PDO::FETCH_ASSOC);
+if($sql->rowCount() === 0){    
+    $sql = $pdo->query("SELECT * FROM tbl_veiculo");
+    $lista = $sql->fetchall(PDO::FETCH_ASSOC);
 
-$sql=$pdo->query("SELECT e.data, e.hora, c.nome , c.contato , v.tipo , v.modelo, v.placa , e.status, u.nome as operador
-FROM tbl_usuario as u INNER JOIN tbl_estacionamento as e on u.id = e.cliente_id 
-INNER JOIN tbl_cliente as c INNER JOIN tbl_veiculo as v on v.cliente_id = c.id");
+    $sql=$pdo->query("SELECT e.data, e.hora, c.nome , c.contato , v.tipo , v.modelo, v.placa , e.status, u.nome as operador
+    FROM tbl_usuario as u INNER JOIN tbl_estacionamento as e on u.id = e.cliente_id 
+    INNER JOIN tbl_cliente as c INNER JOIN tbl_veiculo as v on v.cliente_id = c.id");
 
-$data = filter_input (INPUT_POST, 'data');
-$hora = filter_input (INPUT_POST, 'hora');
-$cliente = filter_input (INPUT_POST, 'cliente');
-$contato = filter_input (INPUT_POST, 'contato');
-$veiculo = filter_input (INPUT_POST, 'veiculo');
-$placa = filter_input (INPUT_POST, 'placa');
-$modelo = filter_input (INPUT_POST, 'modelo');
-$status = filter_input (INPUT_POST, 'status');
+    $data = filter_input (INPUT_POST, 'data');
+    $hora = filter_input (INPUT_POST, 'hora');
+    $cliente = filter_input (INPUT_POST, 'cliente');
+    $contato = filter_input (INPUT_POST, 'contato');
+    $veiculo = filter_input (INPUT_POST, 'veiculo');
+    $placa = filter_input (INPUT_POST, 'placa');
+    $modelo = filter_input (INPUT_POST, 'modelo');
+    $status = filter_input (INPUT_POST, 'status');
+};
 
 ?>
 
@@ -37,14 +39,7 @@ $status = filter_input (INPUT_POST, 'status');
         
     <?php foreach($lista as $usuario): ?>
     <tr>
-        <td> <?php echo $usuario['data']; ?> </td>
-        <td> <?= $usuario['hora']; ?> </td>
-        <td> <?= $usuario['cliente']; ?> </td>
-        <td> <?= $usuario['contato']; ?> </td>
-        <td> <?= $usuario['veiculo']; ?> </td>
-        <td> <?= $usuario['placa']; ?> </td>
-        <td> <?= $usuario['modelo']; ?> </td>
-        <td> <?= $usuario['status']; ?> </td>
+    <td> <?php echo $usuario['data']; ?> </td>
     </tr>   
     <?php endforeach; ?>
     
