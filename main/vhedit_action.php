a18d9162894f6511a50063f794cc626eb6863b39
quebec <85215097+jcvieiraa@users.noreply.github.com>
2022-11-16 21:20:47 -0300

    Update vhedit_action.php

--- main/vhedit_action.php
+++ main/vhedit_action.php
@@ -1,44 +1,64 @@
 <?php
 
-    require 'config.php';
+require 'config.php';
 
-    $query = [];
+session_start();
+ob_start();
 
     $dados = filter_input_array (INPUT_POST, FILTER_DEFAULT); 
 
-    $idUsuario = $_SESSION['id'];
+    $operador_id = $_SESSION['id'];
 
-    $nome = filter_input(INPUT_POST, 'nome');
-    $placa = filter_input(INPUT_POST, 'placa');
-    $placa = filter_input(INPUT_POST, 'placa');  
-    $tipo = filter_input(INPUT_POST, 'tipo');
-    $contato = filter_input(INPUT_POST, 'contato');
-    $marca = filter_input(INPUT_POST, 'marca');
-    $modelo = filter_input(INPUT_POST, 'modelo');
-    $data = filter_input(INPUT_POST, 'data');
-    $hora = filter_input(INPUT_POST, 'hora');
+    if($dados) {
 
-    if($id && $nome && $contato && $idade && $placa && $endereco) { 
+        $sql=$pdo->prepare("SELECT * from tbl_cliente WHERE nome = :nome and contato = :contato");
 
-        $sql = $pdo->prepare( "UPDATE SET tbl_cliente (nome, placa) VALUES (:nome, :placa);
-        UPDATE SET tbl_veiculo (tipo, placa, marca, modelo) VALUES(:tipo, :placa, :marca, :modelo);
-        UPDATE SET tbl_estacionamento (data, hora) VALUES(:data, :hora) WHERE `tbl_clientes`.`id` = :id;" );
         $sql->bindValue(':nome', $dados['nome']);
-        $sql->bindValue(':placa', $dados['placa']);
-        $sql->bindValue(':tipo', $dados['tipo']);
         $sql->bindValue(':contato', $dados['contato']);
-        $sql->bindValue(':marca', $dados['marca']);
-        $sql->bindValue(':modelo', $dados['modelo']);
-        $sql->bindValue(':data', $dados['data']);
-        $sql->bindValue(':hora', $dados['hora']);
-        $sql->execute();
+        $sql->execute();    
+        
+        if($sql->rowCount() === 0) {
 
-        header("Location: home.php"); //redirecionamento se sim
-        exit;
+            $sql=$pdo->prepare( "UPDATE tbl_cliente SET nome = :nome, contato = :contato" );
+    
+            $sql->bindValue(':nome', $dados['nome']);
+            $sql->bindValue(':contato', $dados['contato']);
+            $sql->execute();
+    
+            $query=$pdo->prepare("SELECT id from tbl_cliente WHERE nome = :nome and contato = :contato");
+            $query->bindValue(':nome', $dados['nome']);
+            $query->bindValue(':contato', $dados['contato']);
+            $query->execute();
+    
+            if ($query->rowCount() > 0) {
 
-    } else {
-        header("Location: vhedit.php"); //redirecionamento se não
-        exit;
+                $cliente_id = $query->fetch(PDO::FETCH_ASSOC);  
+
+                $sql=$pdo->prepare( "UPDATE tbl_veiculo SET tipo = :tipo, marca = :marca, modelo = :modelo, placa = :placa, cliente_id = :cliente_id" );
+        
+                $sql->bindValue(':tipo', $dados['tipo']);
+                $sql->bindValue(':marca', $dados['marca']);
+                $sql->bindValue(':modelo', $dados['modelo']);
+                $sql->bindValue(':placa', $dados['placa']);
+                $sql->bindValue(':cliente_id', $cliente_id['id']);
+                $sql->execute();
+                
+                $sql=$pdo->prepare("UPDATE tbl_estacionamento SET cliente_id = :cliente_id, operador_id = :operador_id, data = :data, hora = :hora");
+                
+                $sql->bindValue(':cliente_id', $cliente_id['id']);
+                $sql->bindValue(':operador_id', $operador_id);
+                $sql->bindValue(':data', $dados['data']);
+                $sql->bindValue(':hora', $dados['hora']);
+                $sql->execute();
+
+                header("Location: home.php");
+                exit;
+
+            }  
+
+        } else {
+            header("Location: vhedit.php");
+            exit;
+        }
     }
-
 ?>
