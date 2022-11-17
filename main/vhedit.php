90976671190b9d9f56c323d9db4f37f725dcfbf8
quebec <85215097+jcvieiraa@users.noreply.github.com>
2022-11-16 21:20:34 -0300

    Update vhedit.php

--- main/vhedit.php
+++ main/vhedit.php
@@ -1,9 +1,27 @@
 <?php 
 
-require 'config.php';
+    session_start();
+    ob_start();
 
-session_start();
-ob_start();
+    require 'config.php';
+
+$dados = [];
+
+$id = filter_input(INPUT_GET, 'id');  //Filtra os valores de ID no fomulário.
+
+    if($id) {
+        
+        
+        $sql=$pdo->query("SELECT e.id,  e.data, e.hora, c.nome , c.contato , v.tipo , v.modelo, v.placa , e.status, u.nome as operador
+        FROM tbl_usuario as u INNER JOIN tbl_estacionamento as e on u.id = e.operador_id 
+        INNER JOIN tbl_cliente as c on e.cliente_id = c.id INNER JOIN tbl_veiculo as v on v.cliente_id = c.id WHERE e.id = $id");
+
+        if($sql->rowCount() > 0) {
+            $dados = $sql->fetch(PDO::FETCH_ASSOC);
+            print_r($dados);
+        }       
+    }
+
 
 ?>
 
@@ -41,31 +59,27 @@ ob_start();
 
             <div id="form-container">
 
-                <form action="vhregister_action.php" method="post">
+                <form action="vhedit_action.php" method="post">
 
                     <input type="hidden" name="operador"> <br>
 
-                    <input class="input-form" type="text" name="nome"> <br>
+                    <input class="input-form" type="text" name="nome" value="<?php echo $dados['nome']; ?>"> <br>
 
-                    <input class="input-form" type="text" name="contato"> <br>
+                    <input class="input-form" type="text" name="contato" value="<?php echo $dados['contato']; ?>"> <br>
 
-                    <input class="input-form" type="number" name="tipo"> <br>
+                    <input class="input-form" type="number" name="tipo" value="<?php echo $dados['tipo']; ?>"> <br>
 
-                    <input class="input-form" type="text" name="placa"> <br>
+                    <input class="input-form" type="text" name="placa" value="<?php echo $dados['placa']; ?>"> <br>
 
-                    <input class="input-form" type="text" name="marca"> <br>
+                    <input class="input-form" type="text" name="marca" value="<?php echo $dados['marca']; ?>"> <br>
 
-                    <input class="input-form" type="text" name="modelo"> <br>
+                    <input class="input-form" type="text" name="modelo" value="<?php echo $dados['modelo']; ?>"> <br>
 
-                    <input class="input-form" type="date" name="data"> <br>
-
-                    <input class="input-form" type="time" name="hora"> <br>
-
-                    <input id="button-form" type="submit" name="cadastrar" value="cadastrar">
+                    <input id="button-form" type="submit" name="edit" value="editar">
             
                 </form>
 
-                <div> <a href="home.php"> Voltar </a>  </div>
+                <div> <a href="home.php"> Cancelar </a> </div>
                 
             </div>    
                                
